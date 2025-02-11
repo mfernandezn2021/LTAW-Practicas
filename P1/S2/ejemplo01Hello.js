e2ca5419a07aabda8d60bdc757c2e8cb13444980
mario <m.fernandezn.2021@alumnos.urjc.es>
2025-02-11 14:08:48 +0100

    Modificaciones javascript

--- P1/S2/ejemplo01Hello.js
+++ P1/S2/ejemplo01Hello.js
@@ -1,1 +1,11 @@
+let n = 3; 
+
 console.log("Hola Mundo");
+
+// Concatenacion de cadenas con variables
+console.log("Variable n = ", n);
+
+console.log("Variable n = " + n);
+
+console.log(`Variable n = ${n}`);
+
