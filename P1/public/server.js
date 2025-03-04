f8f1660f7862ccf79fd16cb537faf616263f37be
mfdezn <m.fernandezn.2021@alumnos.urjc.es>
2025-03-04 14:28:29 +0100

    Algunos avances

--- P1/public/server.js
+++ P1/public/server.js
@@ -4,15 +4,19 @@ const path = require('path');
 const app = express();
 const PORT = 3000; // Puedes cambiar el puerto si lo necesitas
 
+// Ruta al directorio public
+const publicPath = '/home/cosmiccondor/Documentos/cuartoCurso/segundoCuatri/LTAW-Practicas/P1/public';
+
 // Middleware para servir archivos estáticos desde la carpeta "public"
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(publicPath));
 
 // Ruta principal (sirve el archivo index.html)
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 // Iniciar el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Sirviendo archivos estáticos desde: ${publicPath}`);
 });
