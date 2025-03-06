284c41f5112c74d94fa8eeec5cdc18a4b008bd9d
mfdezn <m.fernandezn.2021@alumnos.urjc.es>
2025-03-06 23:12:42 +0100

    Server tienda http

---
+++ P1/tienda.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 2000;
+
+const publicPath = path.join(__dirname, 'public');
+
+app.use(express.static(publicPath));
+
+// Ruta para la página principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+// Ruta para las páginas de productos
+app.get('/producto/:id', (req, res) => {
+    const productoId = req.params.id;
+    const productoPath = path.join(publicPath, 'productos', `producto${productoId}.html`);
+    
+    // Verifica si el archivo del producto existe
+    if (require('fs').existsSync(productoPath)) {
+        res.sendFile(productoPath);
+    } else {
+        res.status(404).sendFile(path.join(publicPath, 'error.html'));
+    }
+});
+
+// Manejo de rutas no encontradas
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(publicPath, 'error.html'));
+});
+
+app.listen(PORT, 'localhost',() => {
+    console.log(`Servidor corriendo en http://10.1.157.18:${PORT}`);
+});
